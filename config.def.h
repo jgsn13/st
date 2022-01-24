96fdc709065369fc0b9b93e5ec3c56c909167738
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-24 13:25:14 -0300

    add diferent bg opacity to unfocused windows

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.88;
+float alpha = 0.88, alphaUnfocused = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -136,6 +136,8 @@ unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
+unsigned int focusedbg = 16;
+unsigned int unfocusedbg = 16;
 
 /*
  * Default shape of cursor

da2c9319d3c2a32d7ad943e46a179c9212abc01f
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-07-31 17:05:41 -0300

    feat: update word delimiters

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono NF:style:regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF:style:regular:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -34,7 +34,7 @@ static float chscale = 1.0;
  *
  * More advanced example: L" `'\"()[]{}:7"
  */
-wchar_t *worddelimiters = L" ";
+wchar_t *worddelimiters = L" `'\"()[]{}:7";
 
 /* selection timeouts (in milliseconds) */
 static unsigned int doubleclicktimeout = 300;
